38de0cf62c29ef441adc19b06dcba2f3295c25b5
Pawan Bhayde <pawanbhayde721@gmail.com>
2024-05-17 21:30:36 +0530

    Remove BorderBeam component and update HeroImage styling

--- components/heroimage.tsx
+++ components/heroimage.tsx
@@ -1,13 +1,9 @@
-import { BorderBeam } from "@/components/ui/border-beam";
-
 export function HeroImage() {
   return (
-    <div className="relative rounded-xl">
-      <video className="border rounded-xl" autoPlay muted loop>
+    <div className="relative rounded-xl w-full">
+      <video className="border rounded-xl w-full" autoPlay muted loop>
         <source src="/videos/dashboard.mp4" type="video/mp4" />
       </video>
-
-      <BorderBeam size={250} duration={12} delay={9} />
     </div>
   );
 }
