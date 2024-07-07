5853539435f5a76079dbb4eefb3cece8c56b0e26
Pawan Bhayde <pawanbhayde721@gmail.com>
2024-07-07 23:53:49 +0530

    feat: Update login redirect logic
    Refactor login page to redirect users to the home page after successful login. This improves the user experience by providing a seamless transition to the main application.

--- app/dashboard/articles/create/page.tsx
+++ app/dashboard/articles/create/page.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import dynamic from "next/dynamic";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import {
   Select,
   SelectContent,
@@ -11,22 +11,120 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { supabase } from "@/lib/supabaseClient";
+import { useUser } from "@/lib/useUser";
 
 const JoditEditor = dynamic(() => import("jodit-react"), { ssr: false });
+
+type Author = {
+  id: number;
+  name: string;
+  image_url: string;
+  instagram: string;
+  twitter: string;
+};
+
+type Category = {
+  id: number;
+  category_name: string;
+};
 
 const CreateArticle = () => {
   const editor = useRef(null);
   const [content, setContent] = useState("");
   const [htmlContent, setHtmlContent] = useState("");
+  const [title, setTitle] = useState("");
+  const [subtitle, setSubtitle] = useState("");
+  const [slug, setSlug] = useState("");
+  const [keywords, setKeywords] = useState("");
+  const [coverImage, setCoverImage] = useState<File | null>(null);
+  const [authors, setAuthors] = useState<Author[]>([]);
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [selectedAuthor, setSelectedAuthor] = useState<number | null>(null);
+  const [selectedCategory, setSelectedCategory] = useState<number | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const { user } = useUser();
 
   const handleEditorChange = (newContent: string) => {
     setContent(newContent);
     setHtmlContent(newContent);
   };
 
-  const handleSubmit = (event: React.FormEvent) => {
+  useEffect(() => {
+    const fetchAuthors = async () => {
+      const { data, error } = await supabase.from("authors").select("*");
+      if (error) console.error("Error fetching authors:", error);
+      else setAuthors(data as Author[]);
+    };
+
+    const fetchCategories = async () => {
+      const { data, error } = await supabase.from("categories").select("*");
+      if (error) console.error("Error fetching categories:", error);
+      else setCategories(data as Category[]);
+    };
+
+    fetchAuthors();
+    fetchCategories();
+  }, []);
+
+  const generateSlug = (title: string) => {
+    return title
+      .toLowerCase()
+      .replace(/ /g, "-")
+      .replace(/[^\w-]+/g, "");
+  };
+
+  const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-    // Handle form submission here, e.g., send content to a server
+    const slug = generateSlug(title);
+    if (!user || !title) return;
+
+    let coverImageUrl: string | null = null;
+
+    if (coverImage) {
+      const { data: uploadData, error: uploadError } = await supabase.storage
+        .from("author-images")
+        .upload(`${user.id}/${coverImage.name}`, coverImage);
+
+      if (uploadError) {
+        console.error("Error uploading image:", uploadError);
+        setLoading(false);
+        return;
+      }
+      console.log("uploadData", uploadData);
+
+      const publicUrl = supabase.storage
+        .from("author-images")
+        .getPublicUrl(uploadData.path).data.publicUrl;
+      coverImageUrl = publicUrl || null;
+    }
+
+    const { error } = await supabase.from("articles").insert([
+      {
+        title,
+        subtitle,
+        slug,
+        keywords: keywords.split(",").map((keyword) => keyword.trim()),
+        cover_image_url: coverImageUrl,
+        author_id: selectedAuthor,
+        category_id: selectedCategory,
+        content,
+      },
+    ]);
+
+    if (error) {
+      console.error("Error creating article:", error);
+    } else {
+      console.log("Article created successfully");
+      setTitle("");
+      setSubtitle("");
+      setKeywords("");
+      setCoverImage(null);
+      setSelectedAuthor(null);
+      setSelectedCategory(null);
+      setContent("");
+      setHtmlContent("");
+    }
   };
 
   return (
@@ -34,7 +132,13 @@ const CreateArticle = () => {
       <form onSubmit={handleSubmit}>
         <div className="mb-4">
           <label htmlFor="title">Title</label>
-          <Input placeholder="Enter title here" type="text" name="title" />
+          <Input
+            placeholder="Enter title here"
+            type="text"
+            name="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div className="mb-4">
           <label htmlFor="subtitle">Subtitle</label>
@@ -42,46 +146,70 @@ const CreateArticle = () => {
             placeholder="Enter subtitle here"
             type="text"
             name="subtitle"
+            value={subtitle}
+            onChange={(e) => setSubtitle(e.target.value)}
           />
         </div>
         <div className="mb-4 flex gap-10">
           <div className="flex-1">
             <label htmlFor="slug">Slug</label>
-            <Input placeholder="Enter slug here" type="text" name="slug" />
+            <Input
+              placeholder="Enter slug here"
+              type="text"
+              name="slug"
+              value={slug}
+              onChange={(e) => setSlug(e.target.value)}
+            />
           </div>
           <div className="flex-1">
-            <label htmlFor="slug">Keywords</label>
-            <Input placeholder="Enter keywords here" type="text" name="slug" />
+            <label htmlFor="keywords">Keywords</label>
+            <Input
+              placeholder="Enter keywords here"
+              type="text"
+              name="keywords"
+              value={keywords}
+              onChange={(e) => setKeywords(e.target.value)}
+            />
           </div>
         </div>
         <div className="flex w-full gap-10">
           <div className="flex-1">
             <label htmlFor="coverimage">Cover Image</label>
-            <Input type="file" name="coverimage" />
+            <Input
+              type="file"
+              name="coverimage"
+              onChange={(e) => setCoverImage(e.target.files?.[0] ?? null)}
+            />
           </div>
           <div className="flex-1">
             <label htmlFor="author">Author</label>
-            <Select>
+            <Select onValueChange={(value) => setSelectedAuthor(Number(value))}>
               <SelectTrigger className="">
                 <SelectValue placeholder="Select Author" />
               </SelectTrigger>
               <SelectContent>
-                <SelectItem value="johndoe">John Doe</SelectItem>
-                <SelectItem value="cris">Cris Doe</SelectItem>
-                <SelectItem value="von">Von Doe</SelectItem>
+                {authors.map((author) => (
+                  <SelectItem key={author.id} value={String(author.id)}>
+                    {author.name}
+                  </SelectItem>
+                ))}
               </SelectContent>
             </Select>
           </div>
           <div className="flex-1">
             <label htmlFor="category">Category</label>
-            <Select>
+            <Select
+              onValueChange={(value) => setSelectedCategory(Number(value))}
+            >
               <SelectTrigger className="">
                 <SelectValue placeholder="Select Category" />
               </SelectTrigger>
               <SelectContent>
-                <SelectItem value="Development">Development</SelectItem>
-                <SelectItem value="Design">Design</SelectItem>
-                <SelectItem value="Roadmaps">Roadmaps</SelectItem>
+                {categories.map((category) => (
+                  <SelectItem key={category.id} value={String(category.id)}>
+                    {category.category_name}
+                  </SelectItem>
+                ))}
               </SelectContent>
             </Select>
           </div>
@@ -95,7 +223,7 @@ const CreateArticle = () => {
           />
         </div>
         <Button className="absolute top-20 right-10" type="submit">
-          Publish
+          {loading ? "Publishing..." : "Publish"}
         </Button>
       </form>
       <div className="mt-10">
